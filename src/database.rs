1111996735222e77fc09d1a0977a608969e856bc
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2024-12-18 21:55:13 +0800

    add entry code

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,6 @@
 pub mod database {
     use duckdb::{params, Connection};
+    use chrono::{Utc, TimeZone};
 
     pub struct DatabaseHandler {
         conn: Connection,
@@ -8,22 +9,77 @@ pub mod database {
     impl DatabaseHandler {
         pub fn new() -> Self {
             DatabaseHandler {
-                conn: Connection::open_in_memory().expect("Failed to open connection"),
+                conn: Connection::open("worklog.db").expect("Failed to open connection"),
             }
         }
         pub fn init_handler(&self) {
             self.conn
                 .execute_batch(
-                    r"CREATE SEQUENCE seq;
-          CREATE TABLE salary (
-                  id             INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq'),
-                  date           TEXT NOT NULL,
-                  entry          TEXT,
-                  exit           TEXT
+                    r"CREATE TABLE IF NOT EXISTS worklog (
+                  date           INTEGER PRIMARY KEY,
+                  entry          INTEGER,
+                  exit           INTEGER
                   );
          ",
                 )
                 .expect("Failed to create table");
+        }
+        pub fn add_entry(&self, midnight:u64, time: u64) {
+            self.conn
+                .execute(
+                    "INSERT INTO worklog (date, entry, exit) VALUES (?, ?, ?)  ON CONFLICT DO UPDATE SET entry = EXCLUDED.entry;",
+                    params![midnight, time, 0u64],
+                )
+                .expect("Failed to insert worklog entry");
+            println!("Added entry. Good luck on staring ur day fresh");
+        }
+        pub fn add_exit(&self, midnight:u64, time: u64) {
+            self.conn
+                .execute(
+                    "INSERT INTO worklog (date, entry, exit) VALUES (?, ?, ?) ON CONFLICT DO UPDATE SET exit = EXCLUDED.exit;",
+                    params![midnight, 0u64, time],
+                )
+                .expect("Failed to insert worklog exit");
+            println!("Added exit. Enjoy your leave");
+        }
+
+        pub fn pretty_print(&self, midnight:u64, entry: u64, exit: u64) {
+            let midnight_utc = Utc.timestamp_opt(midnight.try_into().unwrap(), 0).unwrap();
+            let midnight_local = midnight_utc.with_timezone(&chrono::Local);
+            let entry_utc = Utc.timestamp_opt(entry.try_into().unwrap(), 0).unwrap();
+            let entry_local = entry_utc.with_timezone(&chrono::Local);
+            let exit_utc = Utc.timestamp_opt(exit.try_into().unwrap(), 0).unwrap();
+            let exit_local = exit_utc.with_timezone(&chrono::Local);
+            println!("Date: {} Entry: {} Exit {}", midnight_local, entry_local, exit_local);
+            println!("Log:");
+            println!("No work logged for this day :)");
+        }
+
+        pub fn print(&self) {
+            #[derive(Debug)]
+            struct WorkLog {
+                date: u64,
+                entry: u64,
+                exit: u64,
+            }
+            let mut stmt = self
+                .conn
+                .prepare("SELECT date, entry, exit FROM worklog")
+                .expect("Failed to get data");
+            let log_iter = stmt
+                .query_map([], |row| {
+                    Ok(WorkLog {
+                        date: row.get(0)?,
+                        entry: row.get(1)?,
+                        exit: row.get(2)?,
+                    })
+                })
+                .expect("Query failed");
+
+            for log in log_iter {
+                let value = log.unwrap();
+                self.pretty_print(value.date, value.entry, value.exit)
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
 use clap::Parser;
 pub mod database;
 use database::database::DatabaseHandler;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Parser)]
 struct Cli {
-    entry: Option<String>,
-    exit: Option<String>,
+    command: Option<String>,
 }
 
-fn add_entry(value: String) {
-
+fn get_time() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_secs()
 }
 
-fn add_exit(value:String) {
+fn get_midnight(time: u64) -> u64 {
+    let seconds_in_day = 24 * 60 * 60;
+    let secs_from_midnight = time % seconds_in_day;
+    time - secs_from_midnight
+}
 
+fn add_entry(handler: &DatabaseHandler) {
+    let time = get_time();
+    handler.add_entry(get_midnight(time), time);
+}
+
+fn add_exit(handler: &DatabaseHandler) {
+    let time = get_time();
+    handler.add_exit(get_midnight(time), time);
 }
 
 fn main() {
     let database_handler = DatabaseHandler::new();
     database_handler.init_handler();
     let args = Cli::parse();
-    println!("{:?} {:?}", args.entry, args.exit);
 
-    match args.entry {
-        None => {},
-        Some(value) => add_entry(value),
+    match args.command {
+        None => {}
+        Some(value) => match value.as_str() {
+            "entry" => add_entry(&database_handler),
+            "exit" => add_exit(&database_handler),
+            "print" => database_handler.print(),
+            _ => {}
+        },
     }
-
-    match args.exit {
-        None => {},
-        Some(value) => add_exit(value),
-    }
+    database_handler.print();
 }
